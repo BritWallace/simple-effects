0ad719cab6d06d26577a55b71ed6b43efaf02ae0
Brit Wallace <britwallace888@gmail.com>
2021-10-29 20:30:35 -0700

    Added some HTML, CSS, and JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
-jQuery("h1").click(function() {
-  alert("This is a heading.");
-});
 
-jQuery("p").click(function() {
-  alert("This is a paragraph.");
-});
 
-jQuery("img").click(function() {
-  alert("This is an image.");
+$(document).ready(function() {
+  $("p").click(function() {
+    $(".walrus-showing").toggle();
+    $(".walrus-hidden").toggle();
+  });
+});
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $(".walrus-showing").toggle();
+    $(".walrus-hidden").toggle();
+  });
 });
